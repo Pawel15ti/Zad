9f5858627358a281d70fdd566e26b04e9d74db40
Pawcio <Pawcio@Pawcio-Komputer>
2016-05-30 09:58:20 +0200

    fghgfgjhg

---
+++ AA/src/test/java/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Licz licz=new Licz();
+		System.out.println(licz);
+		
+		Licz l = new Licz();
+		System.out.println(l.liczymy(3, 4));
+
+	}
+
+}
